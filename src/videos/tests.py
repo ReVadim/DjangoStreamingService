a927881eeea239c7d3397037edbac7a3021c4a8a
ReVadim <revani@yandex.ru>
2023-03-22 22:15:36 +0300

    add test model

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Video
 
-# Create your tests here.
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='Test title', description='Test description')
+
+    def test_valid_title(self):
+        title = 'Test title'
+        queryset = Video.objects.filter(title=title)
+        self.assertTrue(queryset.exists())
