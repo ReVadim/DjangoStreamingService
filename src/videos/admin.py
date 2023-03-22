c0df38194e32670db8a7e843fc568bc98fedd46a
ReVadim <revani@yandex.ru>
2023-03-22 22:16:35 +0300

    add proxy model and customize admin

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import VideoAllProxy, VideoPublishedProxy
 
-# Register your models here.
+
+class VideoAllAdmin(admin.ModelAdmin):
+    """ Admin model class Video
+    """
+    list_display = ['title', 'id', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['active']
+    readonly_fields = ['id', 'is_published']
+
+    # def published(self, obj, *args, **kwargs):
+    #     return obj.active
+
+    class Meta:
+        model = VideoAllProxy
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    """ Admin model class Video
+    """
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+
+    class Meta:
+        model = VideoPublishedProxy
+
+
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -8,3 +8,26 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+
+    @property
+    def is_published(self):
+        return self.active
+
+
+class VideoAllProxy(Video):
+    """ Proxy model that displays all videos
+    """
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    """ Proxy model that displays only published videos
+    """
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
