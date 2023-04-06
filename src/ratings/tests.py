9fcbbeef39cdb8621025da9b49f635433acf3d60
ReVadim <revani@yandex.ru>
2023-04-06 22:52:14 +0300

    add new tests

--- src/ratings/tests.py
+++ src/ratings/tests.py
@@ -3,6 +3,7 @@ import random
 from django.contrib.auth import get_user_model
 from src.playlists.models import Playlist
 from src.ratings.models import Rating, RatingChoices
+from django.db.models import Avg
 
 
 User = get_user_model()  # User.objects.all()
@@ -28,15 +29,19 @@ class RatingTestCase(TestCase):
 
     def create_ratings(self):
         items = []
+        self.rating_total = []
         self.rating_count = 200
         for i in range(0, self.rating_count):
             user_obj = self.users.order_by("?").first()
             ply_obj = self.playlists.order_by("?").first()
+            rating_val = random.choice(RatingChoices.choices)[0]
+            if rating_val is not None:
+                self.rating_total.append(rating_val)
             items.append(
                 Rating(
                     user=user_obj,
                     content_object=ply_obj,
-                    value=random.choice(RatingChoices.choices)[0]
+                    value=rating_val
                 )
             )
         Rating.objects.bulk_create(items)
@@ -68,3 +73,16 @@ class RatingTestCase(TestCase):
     def test_rating_random_choices(self):
         value_set = set(Rating.objects.values_list('value', flat=True))
         self.assertTrue(len(value_set) > 1)
+
+    def test_rating_aggregate(self):
+        db_avg = Rating.objects.aggregate(average=Avg('value'))['average']
+        self.assertIsNotNone(db_avg)
+        self.assertTrue(db_avg > 0)
+        total_sum_of_ratings = sum(self.rating_total)
+        result_avg = total_sum_of_ratings / (len(self.rating_total) * 1.0)  # * 1.0 for float value
+        self.assertEqual(result_avg, db_avg)
+
+    def test_rating_playlist_aggregate(self):
+        item_1 = Playlist.objects.aggregate(average=Avg('ratings__value'))['average']
+        self.assertIsNotNone(item_1)
+        self.assertTrue(item_1 > 0)
