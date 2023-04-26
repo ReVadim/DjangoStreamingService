2638439c62635978068670ce1360224b4c9fb03a
ReVadim <revani@yandex.ru>
2023-04-26 22:11:52 +0300

    add RatingForm

---
+++ src/ratings/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from src.ratings.models import RatingChoices
+
+
+class RatingForm(forms.Form):
+    """ Rating form for add rating value
+    """
+    rating = forms.ChoiceField(label='Rate', choices=RatingChoices.choices)
+    object_id = forms.IntegerField(widget=forms.HiddenInput)
+    content_type_id = forms.IntegerField(widget=forms.HiddenInput)
+    next = forms.HiddenInput
+    
