3193fd5de6a837860125bc3d43ee3a0a53f4313d
ReVadim <revani@yandex.ru>
2023-04-27 22:20:02 +0300

    add rating post save form

--- src/ratings/forms.py
+++ src/ratings/forms.py
@@ -9,5 +9,4 @@ class RatingForm(forms.Form):
     rating = forms.ChoiceField(label='Rate', choices=RatingChoices.choices)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
     content_type_id = forms.IntegerField(widget=forms.HiddenInput)
-    next = forms.HiddenInput
-    
+    next = forms.CharField(widget=forms.HiddenInput)
